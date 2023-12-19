b01b60df060998041d20f8b8787b18b31a3cbb2f
Alexander Balla <aiexxash@github.com>
2023-12-19 20:19:52 +0200

    style: - Added LICENSE, README.md and images

---
+++ UAVolunteer/UAVolunteer/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UAVolunteer
+//
+//  Created by Alex Balla on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
