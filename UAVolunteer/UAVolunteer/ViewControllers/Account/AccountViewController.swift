114eea10ec59708fbcfc7e9efc791d56052b568a
Alexander Balla <aiexxash@github.com>
2023-12-20 16:32:14 +0200

    feat: - Connected outlets to "Account VC"

--- UAVolunteer/UAVolunteer/ViewControllers/Account/AccountViewController.swift
+++ UAVolunteer/UAVolunteer/ViewControllers/Account/AccountViewController.swift
@@ -1,9 +1,24 @@
 import UIKit
 
 class AccountViewController: UIViewController {
-
+    @IBOutlet weak var yahooButton: UIButton!
+    @IBOutlet weak var twitterButton: UIButton!
+    @IBOutlet weak var microsoftButton: UIButton!
+    @IBOutlet weak var googleButton: UIButton!
+    @IBOutlet weak var icloudButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    @IBAction func icloudButtonClicked(_ sender: UIButton) {
+    }
+    @IBAction func googleButtonClicked(_ sender: UIButton) {
+    }
+    @IBAction func microsoftButtonClicked(_ sender: UIButton) {
+    }
+    @IBAction func twitterButtonClicked(_ sender: UIButton) {
+    }
+    @IBAction func yahooButtonClicked(_ sender: UIButton) {
     }
 }
