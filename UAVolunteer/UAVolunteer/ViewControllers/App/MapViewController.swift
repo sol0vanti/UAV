93b932a31e2f47a5ac5beedb69743f61937ac42c
Oleksander Balla <aiexxash@github.com>
2023-12-27 17:08:50 +0200

    feat: - Map integration

--- UAVolunteer/UAVolunteer/ViewControllers/App/MapViewController.swift
+++ UAVolunteer/UAVolunteer/ViewControllers/App/MapViewController.swift
@@ -6,24 +6,13 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
