99d003709637a12946737e4cbeb25d94145ece05
aiexxash <ialexballa@gmail.com>
2024-03-23 18:48:00 +0200

    feat: - Long gesture, get location, annotation
    Commit №61

--- UAVolunteer/UAVolunteer/ViewControllers/Main/ProfileScreen/AddAnnotation/SelPlaceViewController.swift
+++ UAVolunteer/UAVolunteer/ViewControllers/Main/ProfileScreen/AddAnnotation/SelPlaceViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import MapKit
 
-class SelPlaceViewController: UIViewController {
+class SelPlaceViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewWillAppear(_ animated: Bool) {
@@ -12,5 +12,23 @@ class SelPlaceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gesture:)))
+        mapView.addGestureRecognizer(longPressGesture)
+        mapView.delegate = self
     }
+    
+    @objc func handleLongPress(gesture: UILongPressGestureRecognizer) {
+            if gesture.state == .began {
+                let point = gesture.location(in: mapView)
+                let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+                print("Latitude: \(coordinate.latitude), Longitude: \(coordinate.longitude)")
+                addAnnotation(at: coordinate)
+            }
+        }
+        
+        func addAnnotation(at coordinate: CLLocationCoordinate2D) {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            mapView.addAnnotation(annotation)
+        }
 }
