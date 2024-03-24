5f5ebdd3fe8ce1b288249f6580f18b9201bdb933
aiexxash <ialexballa@gmail.com>
2024-03-24 12:39:31 +0200

    feat: - Reset & Continue buttons: Add Center
    Commit №63
    - Bug: Background of reset and continue buttons are overlapping.

--- UAVolunteer/UAVolunteer/ViewControllers/Main/ProfileScreen/AddAnnotation/SelPlaceViewController.swift
+++ UAVolunteer/UAVolunteer/ViewControllers/Main/ProfileScreen/AddAnnotation/SelPlaceViewController.swift
@@ -3,12 +3,16 @@ import MapKit
 
 class SelPlaceViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var continueButton: UIButton!
     var pointSet = false
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tabBarController?.title = "Add Center"
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        resetButton.isEnabled = false
+        continueButton.isEnabled = false
     }
 
     override func viewDidLoad() {
@@ -28,6 +32,10 @@ class SelPlaceViewController: UIViewController, MKMapViewDelegate {
                     return
                 }
                 pointSet = true
+                resetButton.isEnabled = true
+                continueButton.isEnabled = true
+                resetButton.backgroundColor = .darkGray
+                continueButton.backgroundColor = .white
                 addAnnotation(at: coordinate)
             }
         }
@@ -35,6 +43,20 @@ class SelPlaceViewController: UIViewController, MKMapViewDelegate {
     func addAnnotation(at coordinate: CLLocationCoordinate2D) {
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
+        annotation.title = "Center"
         mapView.addAnnotation(annotation)
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+        mapView.removeAnnotations(mapView.annotations)
+        pointSet = false
+        resetButton.isEnabled = false
+        continueButton.isEnabled = false
+        continueButton.backgroundColor = .lightGray
+        resetButton.backgroundColor = .clear
+    }
+    
+    @IBAction func continueButtonClicked(_ sender: UIButton) {
+        return
     }
 }
