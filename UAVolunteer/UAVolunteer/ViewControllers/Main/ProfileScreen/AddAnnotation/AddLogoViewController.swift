80377600f0d327d80ed7be9391db1aa023ac7179
aiexxash <ialexballa@gmail.com>
2024-03-27 12:33:16 +0200

    doc: - Delete comments
    Commit №76
    NaN

--- UAVolunteer/UAVolunteer/ViewControllers/Main/ProfileScreen/AddAnnotation/AddLogoViewController.swift
+++ UAVolunteer/UAVolunteer/ViewControllers/Main/ProfileScreen/AddAnnotation/AddLogoViewController.swift
@@ -10,15 +10,6 @@ class AddLogoViewController: UIViewController, PHPickerViewControllerDelegate {
     
     var logo: UIImage?
     let defaults = UserDefaults.standard
-    
-//    var id: String
-//    var address: String
-//    var contact_phone: String
-//    var description: String
-//    var latitude: String
-//    var longitude: String
-//    var name: String
-//    var website: String
 
     override func viewDidLoad() {
         super.viewDidLoad()
